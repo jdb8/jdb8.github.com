4cb5a0ba2066ff62ce756529497532d7bfde2ab0
Joe Bateson <jdb75@cam.ac.uk>
2014-07-04 16:22:41 +0100

    Add Grunt to site build process

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb',
+          force: true
+        }
+      }
+    },
+    uncss: {
+      dist: {
+        files: {
+          'css/main.css': ['index.html']
+        }
+      }
+    },
+    cssmin: {
+      combine: {
+        files: {
+          'css/main.css': ['css/main.css']
+        }
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-uncss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['compass', 'uncss', 'cssmin']);
+
+};
